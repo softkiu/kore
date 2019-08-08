b4d6fd154bf59130174c7bddb6b309d734dc77ed
Eduardo Duran <zakkred1@gmail.com>
2019-08-08 00:22:07 -0500

    fixed path to exec folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# exit if anything returns error code
+# we are interested about git pull, if we fail the update
+# then we should update container image with latest version of the bot
 set -e
 
 cd /openkore
 git pull
 
-exec openkore.pl
+exec /openkore/openkore.pl
